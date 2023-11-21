7195633ab62c728b654206514a3ce7626da353aa
amanji-g <146326514+amanji-g@users.noreply.github.com>
2023-11-21 17:01:03 +0530

    add src

---
+++ src/context/Todocontext.js
@@ -0,0 +1,20 @@
+import { createContext,useContext } from "react";
+export const Todocontext = createContext({
+    todo: [
+        {
+            id: 1,
+            todo:"Todo msg",
+            completed: false
+        },{},{}
+    ],
+   addTodo: (todo) => {},
+   updatedTodo: (id,todo) => {},
+   deleteTodo: (id) => {},
+   toggleComplete: (id) => {}
+})
+
+export const useTodo = ()=>{
+    return useContext(Todocontext)
+}
+
+export  const TodoProvider = Todocontext.Provider
